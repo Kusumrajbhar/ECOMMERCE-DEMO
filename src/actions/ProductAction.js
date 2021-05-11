d5db81f2847361a63fcd720818f97574f334caa7
kusumrajbhar <kusum.rajbhar@neosoftmail.com>
2021-05-11 21:22:55 +0530

    demo

---
+++ src/actions/ProductAction.js
@@ -0,0 +1,21 @@
+import {actionTypes} from './ActionType'
+
+export const setProducts = (products) => {
+    return {
+        type:  actionTypes.SET_PRODUCTS,
+        payload: products,
+    }
+}
+
+export const selectedProducts = (product) => {
+    return {
+        type:  actionTypes.SELECTED_PRODUCT,
+        payload: product,
+    }
+}
+
+export const removeSelectedProduct = () => {
+    return {
+      type: actionTypes.REMOVE_SELECTED_PRODUCT,
+    };
+  };
---
+++ src/component/ProductListing.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import ProductComponent from './ProductComponent'
+import axios from 'axios'
+import {setProducts} from '../actions/ProductAction'
+
+ const ProductListing = () => {
+     const state = useSelector((state) => state.allProducts.products)
+     const dispatch = useDispatch();
+     
+     const fetchProducts = async () => {
+         const response = await axios.get('https://fakestoreapi.com/products')
+         .catch((err)=> {
+             console.log('err', err)
+         })
+        dispatch(setProducts(response.data))
+     }
+
+    useEffect(()=> {
+        fetchProducts()
+    },[])
+
+    return (
+        <div className= 'ui grid container'>
+          <ProductComponent />
+        </div>
+    )
+}
+
+export default ProductListing
